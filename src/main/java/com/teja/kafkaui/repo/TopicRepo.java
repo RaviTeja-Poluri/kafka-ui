4f3028e8c869326ca0ae049efbe3e07d1eec1f3a
RaviTeja1274 <raviteja.poluri@gaiansolutions.com>
2021-04-22 15:20:27 +0530

    added method to get topics list under a server

--- src/main/java/com/teja/kafkaui/repo/TopicRepo.java
+++ src/main/java/com/teja/kafkaui/repo/TopicRepo.java
@@ -1,10 +1,12 @@
 package com.teja.kafkaui.repo;
 
 import com.teja.kafkaui.models.KafkaTopic;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TopicRepo extends MongoRepository<KafkaTopic,String> {
 
+    List<KafkaTopic> findByKafkaServerId(String serverId);
 }
