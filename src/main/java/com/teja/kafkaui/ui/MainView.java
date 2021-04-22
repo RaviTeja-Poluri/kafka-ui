085b494ad48b6aa68736ed076af8988b7f9170a7
RaviTeja1274 <raviteja.poluri@gaiansolutions.com>
2021-04-22 13:45:47 +0530

    added saving kafkaserver to database.

--- src/main/java/com/teja/kafkaui/ui/MainView.java
+++ src/main/java/com/teja/kafkaui/ui/MainView.java
@@ -2,11 +2,27 @@ package com.teja.kafkaui.ui;
 
 import com.teja.kafkaui.models.KafkaServer;
 import com.teja.kafkaui.repo.ServerRepo;
+import com.vaadin.flow.component.AbstractField.ComponentValueChangeEvent;
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.HasValue;
+import com.vaadin.flow.component.HasValue.ValueChangeListener;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.provider.BackEndDataProvider;
+import com.vaadin.flow.data.provider.DataProvider;
+import com.vaadin.flow.data.provider.DataProviderListener;
+import com.vaadin.flow.data.provider.Query;
+import com.vaadin.flow.data.provider.QuerySortOrder;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.shared.Registration;
+import java.util.List;
+import java.util.stream.Stream;
+import org.springframework.util.StringUtils;
 
 @Route
 public class MainView extends VerticalLayout {
@@ -17,10 +33,24 @@ public class MainView extends VerticalLayout {
     public MainView(ServerRepo serverRepo) {
         this.serverRepo = serverRepo;
         this.serverGrid = new Grid<>(KafkaServer.class);
-        Button button = new Button("click", buttonClickEvent -> Notification.show("success"));
-        add(button);
+        add(createLayout());
         add(serverGrid);
         listServers();
+    }
+
+    private HorizontalLayout createLayout(){
+        TextField textField = new TextField("kafka servers", "localhost:9092");
+        final Button button = new Button("add server", buttonClickEvent -> {
+                String value = textField.getValue();
+                if (!StringUtils.hasLength(value)){
+                    value = textField.getPlaceholder();
+                }
+                KafkaServer kafkaServer = new KafkaServer();
+                kafkaServer.setKafkaServers(value);
+                serverRepo.save(kafkaServer);
+                listServers();
+            });
+        return new HorizontalLayout(textField,button);
     }
 
     private void listServers(){
